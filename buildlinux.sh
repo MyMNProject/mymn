d39992b767912e09cbaa66c0d744425907e72321
blademaster <lucieninfy@gmail.com>
2023-04-29 09:14:03 +0200

    [fix]: fix for windows & linux building

--- buildlinux.sh
+++ buildlinux.sh
@@ -1,3 +1,6 @@
 ./autogen.sh
-./configure
+# export LDFLAGS=-L/usr/local/opt/openssl/lib
+# export CPPFLAGS=-I/usr/local/opt/openssl/include
+./configure 
+# --prefix=pwd/depends/x86_64-pc-linux-gnu
 make
